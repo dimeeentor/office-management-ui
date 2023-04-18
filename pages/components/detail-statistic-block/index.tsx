7290e2f1aa56c335cb5dcd90699ad7bdc65bb422
Dmytro Honcharenko <dimeeentor@gmail.com>
2023-04-18 17:07:51 +0200

    now in the "detail-statistic-block" shows appropriate image

--- pages/components/detail-statistic-block/index.tsx
+++ pages/components/detail-statistic-block/index.tsx
@@ -5,20 +5,22 @@ type DetailStatisticBlock = {
     title: string
     statistic: string
     timePeriod: string
+    imageSrcName: string
 }
 
 const DetailStatisticBlock: FC<DetailStatisticBlock> = ({
     title,
     statistic,
     timePeriod,
+    imageSrcName,
 }) => (
     <div className="grid h-64 w-64 rounded-2xl bg-white p-5 shadow">
         <Image
-            src={"/../public/images/detail-statistic-block/engineering.png"}
+            src={`/images/detail-statistic-block/${imageSrcName}.png`}
             alt="engineering_icon"
             height={50}
             width={50}
-            className="text-center"
+            className="aspect-auto text-center"
         />
         <h2 className="justify-self-start font-bold text-blue-800">{title}</h2>
         <hr className="mt-5 pb-5" />
